5884b7bfeff16b1b3b9ec0599e64f7ca8eb8c9b6
Anya Chan <ckayi777@gmail.com>
2024-10-22 21:52:18 +0800

    add Carousel

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,33 +6,30 @@ import {useState, useEffect} from 'react' //React Hook
 import Title from './Title'
 import QuantityBtn from './QuantityBtn'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Image, Nav, Navbar, Container, Row, Col, Button, Alert, Breadcrumb, Cart} from 'react-bootstrap'
+import {Carousel, Image, Nav, Navbar, Container, Row, Col, Button, Alert, Breadcrumb, Cart} from 'react-bootstrap'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 export default function HomePage({ productList }) {
     return (
     <div>
         <Title />
-        <Container fluid>
-            <Row>
-              { productList.map(product => {
+        <Carousel data-bs-theme="dark" interval={1000}>
+        { productList.map(product => {
                 return (
-                  <Col key={product.id} xs={6} sm={4} md={3} className="g-3">
-                      <Link to={"/product/" + product.id}>
-                      <Image src={process.env.PUBLIC_URL + '/img/' + product.image} alt={product.name} className="productBorder" fluid />
-                      <Container>
-                            {product.name}<br/>
-                      </Container>
-                      </Link><br/>
-                        {product.price}<br/>
-                        {product.description}<br/>
-                        <QuantityBtn />
-                  </Col>
+                  <Carousel.Item key={product.id}>
+                    <img
+                      className="d-block w-100"
+                      src={process.env.PUBLIC_URL + '/img/' + product.image} 
+                      alt={product.name}
+                    />
+                    <Carousel.Caption>
+                      <h5>{product.name}</h5>
+                      <p>{product.description}</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
                 )
             })}
-              
-            </Row>
-            </Container>
+    </Carousel>
     </div>
   )
 }
